0d7c9f174311a5e0d2b7405e7a93d81172e92271
Akash10072003 <akashpdy07@gmail.com>
2025-02-12 14:32:50 +0530

    next.config.ts error solve 2

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    // appDir: true, // Required for Next.js App Router (Next.js 13+)
-  },
+  
 };
 
 export default nextConfig;
